a1e732894c8609afb283ad656d94aeb2fd322b23
Noorish Hassan <noorishassan@gmail.com>
2026-03-29 22:24:48 +0200

    delete everything

--- src/content/skills.ts
+++
@@ -1,9 +0,0 @@
-interface Skills {
-  languages: string[];
-  technologies: string[];
-}
-
-export const SKILLS: Skills = {
-  languages: ["JavaScript", "TypeScript", "SQL", "Java", "C/C++", "Python", "CSS/HTML"],
-  technologies: ["Node.js", "NestJS", "Angular", "Vue.js", "Docker", "Kubernetes", "Git", "JIRA", "Linux", "Kafka"]
-} 
